31229c5357675cf8370092acd706fe82d0f2ab94
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2023-01-09 01:07:21 +0000

    testing lint

--- src/components/VoteChoice.js
+++ src/components/VoteChoice.js
@@ -1,25 +1,25 @@
-import { Choice } from "./models";
+import { Choice } from "../models";
 
 export const voteChoices = [
-    {
-        _id: 1,
-        logo: "./assets/tick.svg",
-        choice: Choice.YES,
-        name: "Yes",
-        votes: 0
-    },
-    {
-        _id: 2,
-        logo: "/assets/cross.svg",
-        choice: Choice.NO,
-        name: "No",
-        votes: 0
-    },
-    {
-        _id: 3,
-        logo: "/assets/question.svg",
-        choice: Choice.DONT_KNOW,
-        name: "Dont Know",
-        votes: 0
-    }
-]
+  {
+    _id: 1,
+    logo: "./assets/tick.svg",
+    choice: Choice.YES,
+    name: "Yes",
+    votes: 0,
+  },
+  {
+    _id: 2,
+    logo: "/assets/cross.svg",
+    choice: Choice.NO,
+    name: "No",
+    votes: 0,
+  },
+  {
+    _id: 3,
+    logo: "/assets/question.svg",
+    choice: Choice.DONT_KNOW,
+    name: "Dont Know",
+    votes: 0,
+  },
+];
