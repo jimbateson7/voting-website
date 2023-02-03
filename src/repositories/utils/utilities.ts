7aa702a2b26e0218519c6e9ad1049ce81d57a820
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2023-02-03 16:00:54 +0000

    Improving the embedded video to match the voting page logic (to cope with bad youtube urls)

--- src/repositories/utils/utilities.ts
+++ src/repositories/utils/utilities.ts
@@ -1,7 +1,7 @@
 export const extractYoutubeVideoId = (fullUrl?: string): string => {
     if (!fullUrl) return "Invalid Video";
 
-    //this way someone can add a "watch/embed/share" yt link and it will still work
+    //this way someone can add a "watch/embed/share/the id" yt link and it will still work
     const youtubeId = fullUrl.slice(-11);
 
     return youtubeId;
