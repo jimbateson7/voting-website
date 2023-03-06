c76e212d7b1eab909083efa44be81191f7b128ed
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2023-03-06 11:52:20 +0000

    a bit of clean up

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -6,10 +6,9 @@ import "./Layout.scss";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { footerComponentId, headerComponentId } from "../Routing";
 import {DynamicFooter} from "../components/DynamicFooter";
-import React, {useRef, useState} from "react";
+import React, { useState} from "react";
 
 import {DynamicNavList} from "../components/DynamicNavList";
 
