8357fa5efe42cabc88c902ffcda456307ee5ac61
Tom Jones <contact@tomjones.dev>
2023-03-12 15:57:27 +0000

    Add frames to videos.

--- src/components/VideoEmbed.tsx
+++ src/components/VideoEmbed.tsx
@@ -1,23 +1,36 @@
-import React from "react";
-import {extractYoutubeVideoUrl} from "../repositories/utils/utilities";
+import { extractYoutubeVideoUrl } from "../repositories/utils/utilities";
 
 export type TVideoEmbed = {
-    title: string;
-    url:string |undefined;
-    autoplay:boolean
+  title: string;
+  url: string | undefined;
+  autoplay: boolean;
+  showFrame: boolean;
 };
 
-export const VideoEmbed = (props: TVideoEmbed) =>{
-    
-    const videoUrl = extractYoutubeVideoUrl(props.url ?? "", props.autoplay)
-    return (props.url ? (<div className={"videoIframe"}>
+export const VideoEmbed = (props: TVideoEmbed) => {
+
+  const videoUrl = extractYoutubeVideoUrl(props.url ?? "", props.autoplay)
+
+  return (props.showFrame
+    ? (
+      <div className="videoIframe">
         <iframe
-            className="video"
-            src={videoUrl}
-            title={props.title}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-        ></iframe>
-    </div>) : null);
+          className="video"
+          src={videoUrl}
+          title={props.title}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen>
+        </iframe>
+      </div>)
+    : (
+      <iframe
+        className="video"
+        src={videoUrl}
+        title={props.title}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen>
+      </iframe>)
+  );
 }
--- src/components/VideoPage.tsx
+++ src/components/VideoPage.tsx
@@ -1,12 +1,12 @@
-﻿import {useCallback, useEffect, useState} from "react";
+﻿import { useCallback, useEffect, useState } from "react";
 import "./Page.scss";
 
-import {getVideoPageJson} from "../repositories/VideoPage/request";
+import { getVideoPageJson } from "../repositories/VideoPage/request";
 export interface TVideoPage {
   header: string;
   videoUrl: string | undefined;
   videoTitle: string;
-  autoPlay:boolean;
+  autoPlay: boolean;
 }
 export type TArticlePage = {
   slug: string;
@@ -21,24 +21,24 @@ export const VideoPage = (props: TArticlePage) => {
 
 
   const [data, setData] = useState<TVideoPage>({
-    header: "...video", videoTitle: "UnknownVideo", videoUrl: "", autoPlay:false
+    header: "...video", videoTitle: "UnknownVideo", videoUrl: "", autoPlay: false
   });
 
   useEffect(() => {
     fetchData().catch(console.error);
-  }, [slug,fetchData]);
+  }, [slug, fetchData]);
 
   return (
-    <>   
+    <>
       <h1>{data.header}</h1>
-      <div className={"videoIframe"}>        
+      <div className="videoIframe">
         <iframe
-            className="video"
-            src={data.videoUrl} //todo autoplay
-            title={data.videoTitle}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
+          className="video"
+          src={data.videoUrl} //todo autoplay
+          title={data.videoTitle}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
         ></iframe>
       </div>
 
