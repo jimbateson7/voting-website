feddd663d796feca5187e5596458d40ff80f5e0b
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2024-07-07 02:19:37 +0100

    api keys finally fixed!

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -14,7 +14,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://ydi3uahqqrawleemeo5djyikp4.appsync-api.eu-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "eu-west-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-7yduilgt5nb7rk5uffrrb5vohi",
+    "aws_appsync_apiKey": "da2-piml7w7owvcd5nc4waqnvx2puu",
     "aws_cognito_identity_pool_id": "eu-west-2:168ae70f-d9ad-40bc-8395-0623af7bc149",
     "aws_cognito_region": "eu-west-2",
     "aws_user_pools_id": "eu-west-2_AJymKfucG",
