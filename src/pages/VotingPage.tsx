1d5d8ae68241df89bfe4d0b91072b2de5c1624f4
Chris Pepper <chrispepper1989@gmail.com>
2024-08-21 14:53:20 +0100

    layout changes

--- src/pages/VotingPage.tsx
+++ src/pages/VotingPage.tsx
@@ -157,6 +157,7 @@ const VotingPage = (queryProps: TVotingQueryProps) => {
                                     <span className={"questionTitle"} >
                                     <StructuredText data={question.questionTitleSt}/>
                                     </span>
+                                    
                                     <VoteControls voteCallBack={(b) => setVoted(b)}
                                             
                                                   questionId={question.id}
@@ -169,7 +170,14 @@ const VotingPage = (queryProps: TVotingQueryProps) => {
                                                   onFinish={onWatched} videoThumbnail={props.videoThumbnail?.responsiveImage.src}/>
                                 </Col>
                             </Row>
-
+                            <Row  style={{paddingBottom: "30px"}}>
+                                <Col className="squashToRow"> <SharingControls  voted={voted} shareHeading={props.shareHeading ?? ""}
+                                              shareButtonText={mainQuestionText}/></Col>
+                                <Col className="squashToRow" style={{paddingBottom: "60px"}}>{watched ? <Donation/> : null}</Col>
+                                
+                            </Row>
+                            
+                            
                             {voted && showJumpButtons ? <Row style={{paddingBottom: "30px"}}>
                                 <Col><a href="#share-heading" id="to-share" className="btn btn-primary">Share</a></Col>
                                 <Col><a href="#results-heading" id="to-results" className="btn btn-primary">Results</a></Col>
@@ -183,11 +191,7 @@ const VotingPage = (queryProps: TVotingQueryProps) => {
 
             })}
             <Row style={{marginTop: "-0.5rem"}}>
-                <SharingControls  voted={voted} shareHeading={props.shareHeading ?? ""}
-                                 shareButtonText={mainQuestionText}/>
-                {voted ? <Row style={{paddingBottom: "30px"}}>
-                    {watched ? <Col ><Donation/></Col> : null}
-                </Row> : null}
+                
             </Row>
 
             {voted && showJumpButtons ?
