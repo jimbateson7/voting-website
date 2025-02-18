5210310bd847a039d9e58cd53487ea71504641d6
Chris Pepper <chrispepper1989@gmail.com>
2025-02-18 01:22:49 +0000

    time for sledgehammer

--- src/components/VideoReferenceControl.tsx
+++ src/components/VideoReferenceControl.tsx
@@ -2,7 +2,10 @@ import React, {useEffect, useState} from "react";
 import { ListGroup } from "react-bootstrap";
 import "./VideoControl.scss"
 import "./VideoReferenceControl.scss"
-export type TReferenceProps = {currentTimeStamp: number}
+export type TReferenceProps = {
+    currentTimeStamp: number
+    videoReferenceControlRef: React.RefObject<HTMLElement | null>
+}
 
 interface Reference {
     time: number;
@@ -10,8 +13,8 @@ interface Reference {
     pdfLink: string;
 }
 
-
-const references: Reference[] = [
+/*
+const _references: Reference[] = [
     { time: 14, title: "Should we know?", pdfLink: "https://www.datocms-assets.com/136385/1739562417-1-should-we-know.pdf" },
     { time: 26, title: "What is Hothouse Earth", pdfLink: "https://www.datocms-assets.com/136385/1739562418-2-what-is-hothouse-earth.pdf" },
     { time: 79, title: "The EEI", pdfLink: "https://www.datocms-assets.com/136385/1739562417-3-the-earth-energy-imbalance.pdf" }, //Getting Hotter More Quickly
@@ -20,8 +23,22 @@ const references: Reference[] = [
     { time: 176, title: "Human Emissions", pdfLink: "https://www.datocms-assets.com/136385/1739562417-6-the-investigation.pdf" },
     { time: 182, title: "The Investigation", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, //176
 ];
+*/
+const _references: Reference[] = [
+    { time: 4, title: "Should we know?", pdfLink: "https://www.datocms-assets.com/136385/1739562417-1-should-we-know.pdf" },
+    { time: 6, title: "What is Hothouse Earth", pdfLink: "https://www.datocms-assets.com/136385/1739562418-2-what-is-hothouse-earth.pdf" },
+    { time: 9, title: "The EEI", pdfLink: "https://www.datocms-assets.com/136385/1739562417-3-the-earth-energy-imbalance.pdf" }, //Getting Hotter More Quickly
+    { time: 18, title: "Getting Hotter More Quickly", pdfLink: "https://www.datocms-assets.com/136385/1739562417-4-we-are-getting-hotter-more-quickly.pdf" },
+    { time: 26, title: "Methane", pdfLink: "https://www.datocms-assets.com/136385/1739562417-5-methane.pdf" },
+    { time: 29, title: "Human Emissions", pdfLink: "https://www.datocms-assets.com/136385/1739562417-6-the-investigation.pdf" },
+    { time: 32, title: "The Investigation", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, //176
+];
 
-function findClosestReferenceIndex(currentTimeStamp: number, limit:number = 10): number | undefined {
+function getValidReferences(currentTimeStamp:number, references:Reference[]):Reference[]
+{
+    return references.filter( ref => ref.time < currentTimeStamp);
+}
+function findClosestReferenceIndex(currentTimeStamp: number, references:Reference[], limit:number = 10): number | undefined {
     let low = 0;
     let high = references.length - 1;
     let closestIndex: number | undefined = undefined;
@@ -48,31 +65,71 @@ function findClosestReferenceIndex(currentTimeStamp: number, limit:number = 10):
     return closestIndex;
 }
 
-export const VideoReferenceControl = ({currentTimeStamp}: TReferenceProps) => {
-    const activeIndex =findClosestReferenceIndex(currentTimeStamp) ?? null;
+export const VideoReferenceControl = ({currentTimeStamp, videoReferenceControlRef}: TReferenceProps) => {
+    const activeIndex =findClosestReferenceIndex(currentTimeStamp,_references) ?? null;
+    const activeReference: Reference | null = activeIndex ? _references[activeIndex] : null;
+    var referenceTime = activeReference?.time;
+    const validReferences = getValidReferences(currentTimeStamp, _references).sort((a, b) => b.time - a.time);
 
+    const [fullWidthStyle, setFullWidthStyle] = useState({});
+
+    useEffect(() => {
+        const handleResize = () => {
+
+            handleReferenceControlChange()
+
+        };
+
+        // Add the event listener
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup function: remove the event listener when the component unmounts or the effect re-runs
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []); // Empty dependency array means this effect runs only on mount and unmount
+
+    function handleReferenceControlChange()
+    {
+        if (videoReferenceControlRef.current) {
+            const rect = videoReferenceControlRef.current.getBoundingClientRect();
+            const reversePadding = 0.09;
+            const width = rect.width * (1-reversePadding);
+            const left = rect.left + (rect.width *(reversePadding/2)); 
+            setFullWidthStyle({
+              //  width: width,
+              //  left: left,
+            });
+        } else {
+            setFullWidthStyle({}); // Clear styles when no active reference
+        }
+    }
+    useEffect(() => {
+        handleReferenceControlChange();
+    }, [videoReferenceControlRef]);
+    
     return (
-        <ListGroup style={{height:"20%", overflowY: "auto", overflowX:"hidden"}}> {/* Use ListGroup for better styling */}
-            {activeIndex !=null  ? <ListGroup.Item 
+        <ListGroup style={{ overflowY: "auto", overflowX:"hidden"}}> {/* Use ListGroup for better styling */}
+            {activeReference !=null  ? <ListGroup.Item 
                 key={activeIndex}           
                
                 action // Makes the items clickable
                 active={true} // Sets active state
-                className="text-left  list-group-item-text"
-                
+                className="text-left  list-group-item-text current-reference"
+                style={fullWidthStyle}
                 onClick={() => {
-                    window.open(references[activeIndex].pdfLink, "_blank");
+                    window.open(activeReference.pdfLink, "_blank");
                 }}
                
             >
-                {references[activeIndex].title}
+                🔗{activeReference.title}
             </ListGroup.Item> : null}
             
-            {references.map((ref, indexA) => (
+            {validReferences.map((ref, indexA) => (
                 indexA !== activeIndex ? <ListGroup.Item
                     key={indexA}
 
-                    className="text-left list-group-item-text"
+                    className="text-left list-group-item-text other-reference"
                     action // Makes the items clickable
                     active={indexA === activeIndex} // Sets active state
                     onClick={() => {                      
@@ -80,7 +137,7 @@ export const VideoReferenceControl = ({currentTimeStamp}: TReferenceProps) => {
                     }}
                  
                 >
-                    {ref?.title}
+                    🔗{ref?.title}
                 </ListGroup.Item> : null)
             )}
         </ListGroup>
--- src/pages/VideoWithReference.tsx
+++ src/pages/VideoWithReference.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 import {Col, Row} from "react-bootstrap";
 import {TVideoProps, VideoControl} from "../components/VideoControl";
 import {TReferenceProps, VideoReferenceControl} from "../components/VideoReferenceControl";
@@ -9,13 +9,18 @@ export const VideoWithReference = (props: TVideoProps) => {
 
     const [timeStamp, setTimeStamp] = useState<number>(0);
 
+    const videoReferenceControlRef = useRef(null);
+   
+
+    
+    
     return (
-        <Row>
-            <Col xs={12} md={8} className="video-column" style={{marginLeft:props.leftShift}}>
+        <Row className={"video-reference-control"} ref={videoReferenceControlRef}>
+            <Col xs={12} md={8} className="video-column" style={{marginLeft:props.leftShift}} >
                 <VideoControl {...props} onProgress={setTimeStamp}  />
             </Col>
             <Col xs={12} md={4} className="reference-column" style={{marginLeft:-60}}>
-                <VideoReferenceControl currentTimeStamp={timeStamp} />
+                <VideoReferenceControl currentTimeStamp={timeStamp} videoReferenceControlRef={videoReferenceControlRef} />
             </Col>
         </Row>
     )
