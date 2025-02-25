1099b13811babffc031b8b7c2607d93ec7b19760
James Bateson <jim.bateson@outlook.com>
2025-02-25 11:45:22 +0000

    style: mobile tidy up

--- src/pages/VotingPageVariants/VotingPageMainJourney.tsx
+++ src/pages/VotingPageVariants/VotingPageMainJourney.tsx
@@ -1,7 +1,5 @@
-﻿import "../VotingPage.scss";
-
+﻿import React, {useEffect, useState} from "react";
 import Donation from "../../components/Donation";
-import React, {useEffect, useState} from "react";
 import {SharingControls} from "../../components/SharingControls";
 import {TVotingPageExtended} from "../VotingPage";
 import {Button, Col, Container, Fade, Row} from "react-bootstrap";
@@ -10,12 +8,10 @@ import {Choice} from "../../models";
 import {StructuredText} from "react-datocms";
 import {TStagedFlowProps} from "./TStagedFlowProps";
 import { VideoControl } from "../../components/VideoControl";
-import {TReference, VideoReferenceControl} from "../../components/VideoReferenceControl";
 import {VideoWithReference} from "../VideoWithReference";
 import { getReferences } from "../../repositories/References/request";
 
-
-
+import "../VotingPage.scss";
 
 const StagedFlow = (props: TStagedFlowProps) => {
     const [stage, setStage] = useState(0);
@@ -41,170 +37,138 @@ const StagedFlow = (props: TStagedFlowProps) => {
          nextStage(); // Call the additional function
     };
 
-    if(!questionOne)
-    {
+    if(!questionOne) {
         console.log("no questions")
         return ( <></>);
     }
+
     return (
-        <Container className="frame"
-                   style={{ minHeight: "70vh", position: "relative" }} // Ensures height consistency
-        
-        >
-
-            <Row >
-            <div >
-                <div className="frame-content vote-controls" >
-                    {/* Stage Video */}
-                    <Fade in={stage === videoStage} unmountOnExit>
-                        <div style={{ height: '70vh' }}>
-                            <div className={"verticalFrameCentre"}>
-                            
-                            <VideoControl locale={props.locale} fullScreenOnClick={true}
-                                          datoVideo={props.videos?.thankYouVideo?.video?.video}
-                                          onFinish={() => {
-                                              if (props.watchedCallBack) props.watchedCallBack();
-                                              nextStage();
-                                          }} 
-                                         
-                                          videoThumbnail={props.videos?.thankYouVideo.thumbnailImage?.responsiveImage.src}/>
-                            </div>
-                        </div>
-                    </Fade>
-
-                    <Fade in={stage === openingStage} unmountOnExit>
-                        <div style={{ height: '70vh', paddingRight:"50px"}}>
-                            <div className={"verticalFrameCentre"} >
-                                <div style={{fontSize:"1.5rem"}}>
-                                    <StructuredText data={props.openingText}/>
+        <Container className="frame" style={{ position: "relative" }}>
+                <div>
+                    <div className="frame-content vote-controls">
+                        {/* Stage Video */}
+                        <Fade in={stage === videoStage} unmountOnExit>
+                            <div>
+                                <div className={"verticalFrameCentre"}>
+                                    <VideoControl locale={props.locale} fullScreenOnClick={true}
+                                            datoVideo={props.videos?.thankYouVideo?.video?.video}
+                                            onFinish={() => {
+                                                if (props.watchedCallBack) props.watchedCallBack();
+                                                nextStage();
+                                            }} 
+                                            videoThumbnail={props.videos?.thankYouVideo.thumbnailImage?.responsiveImage.src}/>
                                 </div>
                             </div>
-                        </div>
-                       
-                    </Fade>
+                        </Fade>
 
-                    {/* Stage Questions */}
-                 
-                    <Fade in={stage === questionStage } unmountOnExit>
-                        <Row className={"vote-controls"}>
-                            <Col className={"squashToRow pad50"}>
-    
-                                <QuestionComponent {...props}                                                  
-                                                   {...questionOne}
-                                                   voteChangedCallBack={extendedVoteCallback}/>
-                            </Col>
-    
-                            <Col className={"videoColumn squashToRow squashToRow50"}>
-                                <VideoControl locale={props.locale} fullScreenOnClick={true}
-                                              datoVideo={ props.videos?.prop1?.video?.video  }
-                                                    leftShift={-50}
-                                              onFinish={() => {
-                                                  if (props.watchedCallBack) props.watchedCallBack();
-                                                  nextStage();
-                                              }}
-                                              videoThumbnail={ props.videos?.prop1.thumbnailImage?.responsiveImage.src} />
-                            </Col>
-                        </Row>
-                    </Fade>
-              
-
-                    {/* Stage: Sharing */}
-                    <Fade in={stage === shareStage} unmountOnExit>
-                        <div style={{ height: '70vh' }}>
-                            <div >
-                              
-                                    <SharingControls voted={true} shareHeading="Share this!" shareButtonText="Share Now"/>
-        
-                              
-                            </div>
-                        </div>
-                    </Fade>
-
-                    {/* Stage Details */}
-                 
-                    <Fade in={stage === detailStage} unmountOnExit>
-                        <div style={{ height: '70vh' }}>
-                            <div className={"verticalFrameCentre"}>
-                            <VideoWithReference
-                                references={getReferences(props.videos?.detailVideo?.video.id)}
-                                locale={props.locale}
-                                fullScreenOnClick={true}
-                                datoVideo={props.videos?.detailVideo?.video?.video}
-                                onFinish={() => {
-                                    if (props.watchedCallBack)
-                                        props.watchedCallBack();
-                                    nextStage();
-                                }}
-                                videoThumbnail={props.videos?.detailVideo.thumbnailImage?.responsiveImage.src}
-                                currentTimeStamp={0}/>
-                            </div>
-                        </div>
-                    </Fade>
-                    
-                    
-                    {/* Stage: Donation */}
-                    <Fade in={stage === donateStage} unmountOnExit>
-                        <div style={{ height: '70vh' }}>                           
-                            <div className={"verticalFrameCentre"}>
-                                <div>
-                                    <div style={{textAlign: "center", paddingRight:"30px"}}>
-                                        <StructuredText data={props.donateText}/>
+                        <Fade in={stage === openingStage} unmountOnExit>
+                            <div>
+                                <div className={"verticalFrameCentre"} >
+                                    <div style={{fontSize:"1.5rem"}}>
+                                        <StructuredText data={props.openingText}/>
                                     </div>
-                                     <Donation/>
                                 </div>
                             </div>
-                        </div>
-                    </Fade>
+                        </Fade>
+
+                        {/* Stage Questions */}
+                        <Fade in={stage === questionStage } unmountOnExit>
+                            <Row className={"vote-controls"}>
+                                <div>
+                                    <QuestionComponent {...props}                                                  
+                                                    {...questionOne}
+                                                    voteChangedCallBack={extendedVoteCallback}/>
+                                </div>
+        
+                                <div className={"videoColumn"}>
+                                    <VideoControl locale={props.locale} fullScreenOnClick={true}
+                                                datoVideo={ props.videos?.prop1?.video?.video  }
+                                                        leftShift={-50}
+                                                onFinish={() => {
+                                                    if (props.watchedCallBack) props.watchedCallBack();
+                                                    nextStage();
+                                                }}
+                                                videoThumbnail={ props.videos?.prop1.thumbnailImage?.responsiveImage.src} />
+                                </div>
+                            </Row>
+                        </Fade>
+                
+                        {/* Stage: Sharing */}
+                        <Fade in={stage === shareStage} unmountOnExit>
+                            <div>
+                                <div>
+                                    <SharingControls voted={true} shareHeading="Share this!" shareButtonText="Share Now" />
+                                </div>
+                            </div>
+                        </Fade>
+
+                        {/* Stage Details */}
+                        <Fade in={stage === detailStage} unmountOnExit>
+                            <div>
+                                <div className={"verticalFrameCentre"}>
+                                <VideoWithReference
+                                    references={getReferences(props.videos?.detailVideo?.video.id)}
+                                    locale={props.locale}
+                                    fullScreenOnClick={true}
+                                    datoVideo={props.videos?.detailVideo?.video?.video}
+                                    onFinish={() => {
+                                        if (props.watchedCallBack)
+                                            props.watchedCallBack();
+                                        nextStage();
+                                    }}
+                                    videoThumbnail={props.videos?.detailVideo.thumbnailImage?.responsiveImage.src}
+                                    currentTimeStamp={0}/>
+                                </div>
+                            </div>
+                        </Fade>
+                        
+                        {/* Stage: Donation */}
+                        <Fade in={stage === donateStage} unmountOnExit>
+                            <div>                           
+                                <div className={"verticalFrameCentre"}>
+                                    <div>
+                                        <div style={{textAlign: "center" }}>
+                                            <StructuredText data={props.donateText}/>
+                                        </div>
+                                        <Donation/>
+                                    </div>
+                                </div>
+                            </div>
+                        </Fade>
+                    </div>
                 </div>
-            </div>
-            {/* Navigation Buttons */}
-            <div  className="d-flex justify-content-center gap-3 w-100"
-                  style={{
-                      position: "absolute",
-                      bottom: "-10px",
-                      left: 0,
-                      right: 0,
-                      zIndex: 9999,
-                      textAlign: "center"
-                  }}>
-                <Button variant="secondary" onClick={prevStage} disabled={stage === 0}>
-                    <i className="bi bi-arrow-left">←</i>
-                </Button>
-                <p> {stage+1}/{totalStages}</p>
-                <Button variant="primary" onClick={nextStage} disabled={stage === totalStages - 1}>
-                    <i className="bi bi-arrow-right">→</i>
-                </Button>
-            </div>
-            </Row>
+
+                {/* Navigation Buttons */}
+                <div className="d-flex align-items-center justify-content-center gap-3 w-100 page-navigation">
+                    <Button variant="secondary" onClick={prevStage} disabled={stage === 0}>
+                        <i className="bi bi-arrow-left">←</i>
+                    </Button>
+
+                    <p className="page-navigation__number">{stage+1}/{totalStages}</p>
+
+                    <Button variant="primary" onClick={nextStage} disabled={stage === totalStages - 1}>
+                        <i className="bi bi-arrow-right">→</i>
+                    </Button>
+                </div>
+        
         </Container>
     );
 };
 
-
 export const VotingPageMainJourney = (props: TVotingPageExtended) => {
-
-
     const {voted, watched} = props;
-
     const [showOverlay, setShowOverlay] = useState(false);
 
     useEffect(() => {
-
         if (voted) {
             const targetHeading = document.getElementById('share-heading');
             console.log("targetHeading 2", targetHeading);
             //targetHeading?.scrollIntoView({behavior: 'smooth'});
-        }   
-
-
-    }, [showOverlay]);
+        }}, [showOverlay]);
 
     return (
         <>
            <StagedFlow {...props}></StagedFlow>
         </>
-    )
-        ;
+    );
 };
-
-
