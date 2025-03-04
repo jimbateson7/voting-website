c585c5f5cb3cb5a3489b2c0d8edbf566a1ccda42
Chris Pepper <chrispepper1989@gmail.com>
2025-03-04 13:57:05 +0000

    fix: hardcoded references

--- src/repositories/References/request.ts
+++ src/repositories/References/request.ts
@@ -1,26 +1,31 @@
 import {TReference} from "../../components/VideoReferenceControl";
 
-const _mainVideoReferences: TReference[] = [
-    { time: 14, title: "Should we know?", pdfLink: "https://www.datocms-assets.com/136385/1739562417-1-should-we-know.pdf" },
-    { time: 26, title: "What is Hothouse Earth", pdfLink: "https://www.datocms-assets.com/136385/1739562418-2-what-is-hothouse-earth.pdf" },
-    { time: 79, title: "The EEI", pdfLink: "https://www.datocms-assets.com/136385/1739562417-3-the-earth-energy-imbalance.pdf" }, //Getting Hotter More Quickly
-    { time: 88, title: "Getting Hotter More Quickly", pdfLink: "https://www.datocms-assets.com/136385/1739562417-4-we-are-getting-hotter-more-quickly.pdf" },
-    { time: 116, title: "Methane", pdfLink: "https://www.datocms-assets.com/136385/1739562417-5-methane.pdf" },
-    { time: 176, title: "Human Emissions", pdfLink: "https://www.datocms-assets.com/136385/1739562417-6-the-investigation.pdf" },
-    { time: 182, title: "The Investigation", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, //176
+const updatedVideoReferences = [
+    { time: 19, title: "Should we know?", pdfLink: "https://www.datocms-assets.com/136385/1741096043-4500-1-should-we-know.pdf" },
+    { time: 19, title: "Reasonable", pdfLink: "https://www.datocms-assets.com/136385/1741096043-4500-2-reasonable.pdf" }, // No link provided, setting to null
+    { time: 26, title: "Hothouse Earth?", pdfLink: "https://www.datocms-assets.com/136385/1741096043-4500-3-hothouse-earth.pdf" },
+    { time: 64, title: "The Energy Imbalance", pdfLink: "https://www.datocms-assets.com/136385/1741096043-4500-4-the-eei.pdf" },
+    { time: 85, title: "Getting Hotter More Quickly", pdfLink: "https://www.datocms-assets.com/136385/1741096043-4500-5-getting-hotter-more-quickly.pdf" },
+    { time: 95, title: "Methane", pdfLink: "https://www.datocms-assets.com/136385/1741096043-4500-6-methane.pdf" },
+    { time: 130, title: "The numbers Used", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 153, title: "Aerosols", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 182, title: "The Investigation", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" },
+    { time: 130, title: "Human Emissions", pdfLink: "https://www.datocms-assets.com/136385/1739562417-6-the-investigation.pdf" },
+    { time: 256, title: "Global Net Zero", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 287, title: "Absorption of Carbon", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 309, title: "Melting of Snow and Ice", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 395, title: "Other Factors", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 434, title: "Right Ballpark", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 468, title: "Subsea permafrost", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 488, title: "Clouds", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 549, title: "interactions", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 583, title: "The Shaded area", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 723, title: "10X faster", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 745, title: "Our Planet’s protector", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
+    { time: 767, title: "As soon as possible", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, // No link provided, setting to null
 ];
-/* useful for testing
-const _mainVideoReferences: Reference[] = [
-    { time: 4, title: "Should we know?", pdfLink: "https://www.datocms-assets.com/136385/1739562417-1-should-we-know.pdf" },
-    { time: 6, title: "What is Hothouse Earth", pdfLink: "https://www.datocms-assets.com/136385/1739562418-2-what-is-hothouse-earth.pdf" },
-    { time: 9, title: "The EEI", pdfLink: "https://www.datocms-assets.com/136385/1739562417-3-the-earth-energy-imbalance.pdf" }, //Getting Hotter More Quickly
-    { time: 18, title: "Getting Hotter More Quickly", pdfLink: "https://www.datocms-assets.com/136385/1739562417-4-we-are-getting-hotter-more-quickly.pdf" },
-    { time: 26, title: "Methane", pdfLink: "https://www.datocms-assets.com/136385/1739562417-5-methane.pdf" },
-    { time: 29, title: "Human Emissions", pdfLink: "https://www.datocms-assets.com/136385/1739562417-6-the-investigation.pdf" },
-    { time: 32, title: "The Investigation", pdfLink: "https://www.datocms-assets.com/136385/1739562418-7-anthropogenic.pdf" }, //176
-];*/
 
 export function getReferences(videoId?: string): TReference[]
 {
-    return _mainVideoReferences;
+    return updatedVideoReferences;
 }
