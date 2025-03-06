0b630a12472fa6df8eb84616b3251c045ba4e170
James Bateson <jim.bateson@outlook.com>
2025-03-06 10:53:03 +0000

    style: bit of mobile improvement for the new menu structure

--- src/components/DynamicNavList.tsx
+++ src/components/DynamicNavList.tsx
@@ -83,7 +83,7 @@ export const DynamicNavList = (props: TDynamicNav) => {
                             return (
                                 <NavDropdown title={navItem.title ?? "_"} id={`basic-nav-dropdown-${navItem.id}`}>
                                     <button type="button" className="navigation-back" onClick={(e) => {e.preventDefault(); e.currentTarget.parentElement?.parentElement?.click()}}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" aria-hidden="true"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 320 512" aria-hidden="true"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
                                         Back
                                     </button>
 
@@ -106,7 +106,7 @@ export const DynamicNavList = (props: TDynamicNav) => {
                             return (
                                 <NavDropdown title={navItem.title ?? "_"} id={`basic-nav-dropdown-${navItem.id}`}>
                                     <button type="button" className="navigation-back" onClick={(e) => {e.preventDefault(); e.currentTarget.parentElement?.parentElement?.click()}}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" aria-hidden="true"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 320 512" aria-hidden="true"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
                                         Back
                                     </button>
 
